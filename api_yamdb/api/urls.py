3bfb6564473c0add6452c362d25cd9f3fa69a33d
Андрей Цыбульченко <tsybulchenko@yandex.ru>
2022-05-12 20:07:03 +0300

    1

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,13 +3,14 @@ from rest_framework import routers
 
 from .views import (CategoryViewSet, CommentViewSet,
                     GenreViewSet, ReviewsViewSet,
-                    TitleViewSet)
+                    TitleViewSet, UserViewSet, email, get_jwt_token)
 
 v1_router = routers.DefaultRouter()
 
 v1_router.register(r'categories', CategoryViewSet, basename='categories')
 v1_router.register(r'genres', GenreViewSet, basename='genres')
 v1_router.register(r'titles', TitleViewSet, basename='titles')
+v1_router.register(r'users', UserViewSet, basename='users')
 
 v1_review = v1_router.register(
     r'titles/(?P<title_id>\d+)/reviews',
@@ -21,5 +22,7 @@ v1_comment = v1_router.register(
 )
 
 urlpatterns = [
-    path('v1/', include(v1_router.urls))
+    path('v1/', include(v1_router.urls)),
+    path('v1/auth/email/', email),
+    path('v1/auth/token/', get_jwt_token),
 ]
