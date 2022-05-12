b0fe63c76115f03dc2521a65c680ef7eb8dbea36
Андрей Цыбульченко <tsybulchenko@yandex.ru>
2022-05-12 19:11:14 +0300

    1

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from .models import User
+from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = User
+
+
+class EmailSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+
+
+class ConfirmationSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    confirmation_code = serializers.CharField(required=True)
