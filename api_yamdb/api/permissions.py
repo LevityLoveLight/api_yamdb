4befc3a36c9aeca24c89cfbde3125f6b2172503a
Андрей Цыбульченко <tsybulchenko@yandex.ru>
2022-05-13 03:50:37 +0300

    1

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -22,3 +22,8 @@ class AdminOrReadOnly(permissions.BasePermission):
             request.user.is_authenticated
             and (request.user.is_admin or request.user.is_superuser)
         )
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_admin
